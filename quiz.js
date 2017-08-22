e2d13cc354a87b238b43080b643ba33bfd56b9e2
Devanshu Awasthi <awasthi.devanshu@gmail.com>
2017-08-22 10:10:50 -0700

    created random question generator

--- quiz.js
+++ quiz.js
@@ -1,19 +1,39 @@
-var tempQuestions = ['What is the answer to question 1?',['bunny', 'dog','cat','bird'], 'answer1'];
+var currentQuestion = [];
+
+var allQuestions = [
+    ['What is the answer to question 1?',['bunny', 'dog','cat','bird'], 'answer1'],
+
+    ['What is the answer to question 2?',['billy', 'dog','cat','bird'], 'answer1'],
+
+    ['What is the answer to question 3?',['bob', 'dog','cat','bird'], 'answer1'],
+
+    ['What is the answer to question 4?',['joe', 'dog','cat','bird'], 'answer1']
+];
+
+
+function randomQuestionGen() {
+    currentQuestion = allQuestions [Math.floor(Math.random() * (allQuestions.length))];
+    console.log(currentQuestion);
+};
+
 
 function askQuestion() {
     var answer1Element = document.getElementById( 'answer1' );
-    answer1Element.innerText = tempQuestions[1][0];
+    answer1Element.innerText = currentQuestion[1][0];
 
     var answer2Element = document.getElementById( 'answer2' );
-    answer2Element.innerText = tempQuestions[1][1];
+    answer2Element.innerText = currentQuestion[1][1];
 
     var answer3Element = document.getElementById( 'answer3' );
-    answer3Element.innerText = tempQuestions[1][2];
+    answer3Element.innerText = currentQuestion[1][2];
 
     var answer4Element = document.getElementById( 'answer4' );
-    answer4Element.innerText = tempQuestions[1][3];
+    answer4Element.innerText = currentQuestion[1][3];
 
+    var questionElement = document.getElementById('questions');
+    questionElement.innerText = currentQuestion [0];
 }
+
 // quiz.addEventListener( 'submit', function(){
 //     event.preventDefault();
 //     playerNames.push(this.name.value);
@@ -23,4 +43,5 @@ function askQuestion() {
 //     saveToLocal( 'playerNames', playerNames );
 
 // });
+randomQuestionGen();
 askQuestion();
