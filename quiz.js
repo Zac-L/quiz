abaaeabd95e77ab78ffb48234e494993e5387b9c
mewarren <holla@mewarren.com>
2017-08-22 12:06:03 -0700

    added splice and while loop

--- quiz.js
+++ quiz.js
@@ -1,13 +1,5 @@
 var currentQuestion = [];
-
-// function Player (name, score, consecutiveAnswers) {
-//     this.name = name;
-//     this.score = score;
-//     this.consecutiveAnswers = consecutiveAnswers;
-//     getUserName();
-
-// }
-
+var questionsAsked = 0;
 var player = {
     name: getFromLocal('playerNames'),
     score: 0,
@@ -50,7 +42,7 @@ function askQuestion() {
 
     var el = document.getElementById('question-form');
 
-    //Event listener to check status of correct radio button, return score depending on user input
+     //Event listener to check status of correct radio button, return score depending on user input
     el.addEventListener('submit', function(){
         event.preventDefault();
         var el = document.getElementsByClassName('questionButton');
@@ -61,22 +53,26 @@ function askQuestion() {
         else {
             player.score--;
             console.log('player is wrong');
+
+            questionsAsked++;
         }
         
         event.target.reset();
-        randomQuestionGen();
-        askQuestion();        
+        var spliced = allQuestions.splice( allQuestions.indexOf(currentQuestion), 1 );
+        console.log(spliced);
+        
+        if (allQuestions.length > 0){  
+            randomQuestionGen();
+            askQuestion();  
+        }
+
+        else {alert(player.score);}
+
+        // randomQuestionGen();
+        // askQuestion();  
+              
     });
 }
 
-// quiz.addEventListener( 'submit', function(){
-//     event.preventDefault();
-//     playerNames.push(this.name.value);
-//     console.log(this.name.value);
-//     event.target.reset();
-
-//     saveToLocal( 'playerNames', playerNames );
-
-// });
 randomQuestionGen();
 askQuestion();
