540bd8d61b2ab4b405b64f0b481b803e2a55c156
Robin Miller <rmillerpdx@gmail.com>
2017-08-22 15:10:43 -0700

    add mulit-round code

--- quiz.js
+++ quiz.js
@@ -1,5 +1,7 @@
 var currentQuestion = [];
 var questionsAsked = 0;
+var numberOfRounds = 3;
+var questionsInRound = 2;
 
 var player = {
     name: getFromLocal('playerNames'),
@@ -37,6 +39,8 @@ function randomQuestionGen() {
 };
 
 function askQuestion() {
+
+    console.log('round: ' + numberOfRounds + ' question: ' + ( questionsInRound - questionsAsked ));
     //Populate Label elements with four possible answers to a question
     for (var i = 0; i < 4; i++){
         var answerElement = document.getElementById('answer' + ( i + 1 ));
@@ -56,28 +60,41 @@ el.addEventListener('submit', function(){
     if (el[currentQuestion[2]].checked){
         player.score++;
         console.log('player is right');
+        questionsAsked++;
     }
     else {
         player.score--;
         console.log('player is wrong');
-
         questionsAsked++;
     }
     event.target.reset();
     var spliced = allQuestions.splice( allQuestions.indexOf(currentQuestion), 1 );
-    console.log(spliced);
-    if (questionsAsked < 3 ){
-        randomQuestionGen();
-        askQuestion();
+
+    //If there are still questions in the round
+    if (questionsAsked < questionsInRound ){
+        newQuestion();
     }
 
-    else {
+    //If there are no more rounds left
+    else if (numberOfRounds == 0){
         saveToLocal('playerOne', player);
         console.log(localStorage.playerScore);
         // window.location.href = 'contact.html';
         alert(player.score);
     }
+
+    //If there are no more questions in the round
+    else {
+        numberOfRounds--;
+        questionsAsked = 0;
+        console.log('number of rounds is now ' + numberOfRounds);
+        newQuestion();
+    }
 });
 
-randomQuestionGen();
-askQuestion();
+function newQuestion(){
+    randomQuestionGen();
+    askQuestion();
+}
+
+newQuestion();
