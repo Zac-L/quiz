47950a1ebb6d43b91721cdec130545ec1cd90339
Robin Miller <rmillerpdx@gmail.com>
2017-08-22 10:50:12 -0700

    add event listener to check correct user input

--- quiz.js
+++ quiz.js
@@ -1,21 +1,20 @@
 var currentQuestion = [];
+var playerScore = 0; //Temporary global variable
 
 var allQuestions = [
-    ['What is the answer to question 1?',['bunny', 'dog','cat','bird'], 'answer1'],
+    ['What is the answer to question 1?',['bunny', 'dog','cat','bird'], 0],
 
-    ['What is the answer to question 2?',['billy', 'dog','cat','bird'], 'answer1'],
+    ['What is the answer to question 2?',['billy', 'dog','cat','bird'], 2],
 
-    ['What is the answer to question 3?',['bob', 'dog','cat','bird'], 'answer1'],
+    ['What is the answer to question 3?',['bob', 'dog','cat','bird'], 3],
 
-    ['What is the answer to question 4?',['joe', 'dog','cat','bird'], 'answer1']
+    ['What is the answer to question 4?',['joe', 'dog','cat','bird'], 1]
 ];
-
 
 function randomQuestionGen() {
     currentQuestion = allQuestions [Math.floor(Math.random() * (allQuestions.length))];
     console.log(currentQuestion);
 };
-
 
 function askQuestion() {
     var answer1Element = document.getElementById( 'answer1' );
@@ -32,6 +31,23 @@ function askQuestion() {
 
     var questionElement = document.getElementById('questions');
     questionElement.innerText = currentQuestion [0];
+
+    var el = document.getElementById('question-form');
+
+    //Event listener to check status of correct radio button, return score depending on user input
+    el.addEventListener('submit', function(){
+        event.preventDefault();
+        var el = document.getElementsByClassName('questionButton');
+        if (el[currentQuestion[2]].checked){
+            playerScore++;
+            console.log('player is right');
+        }
+        else {
+            playerScore--;
+            console.log('player is wrong');
+        }
+
+    });
 }
 
 // quiz.addEventListener( 'submit', function(){
