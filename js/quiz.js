7b903b3ee2e4ed283079303381c6f14d80906f44
mewarren <holla@mewarren.com>
2017-08-23 11:09:08 -0700

    added random questions into allQuestions array

--- js/quiz.js
+++ js/quiz.js
@@ -9,18 +9,45 @@ var player = {
 };
 
 var allQuestions = [
-    ['What is the answer to question 1?',['bunny', 'dog','cat','bird'], 0],
-    ['What is the answer to question 2?',['billy', 'dog','cat','bird'], 2],
-    ['What is the answer to question 3?',['bob', 'dog','cat','bird'], 3],
-    ['What is the answer to question 4?',['joe', 'dog','cat','bird'], 1],
-    ['What is the answer to question 5?',['bunny', 'dog','cat','bird'], 0],
-    ['What is the answer to question 6?',['billy', 'dog','cat','bird'], 2],
-    ['What is the answer to question 7?',['bob', 'dog','cat','bird'], 3],
-    ['What is the answer to question 8?',['bob', 'dog','cat','bird'], 3],
-    ['What is the answer to question 9?',['joe', 'dog','cat','bird'], 1],
-    ['What is the answer to question 10?',['bunny', 'dog','cat','bird'], 0],
-    ['What is the answer to question 11?',['billy', 'dog','cat','bird'], 2],
-    ['What is the answer to question 12?',['bob', 'dog','cat','bird'], 3],
+     ['In the year 1900 in the U.S. what were the most popular first names given to boy and girl babies?', [ 'William and Elizabeth', 'Joseph and Catherine', 'John and Mary', 'George and Anne' ], 2 ],
+
+    [ 'When did the Liberty Bell get its name?', [ 'when it was made, in 1701', 'when it rang on July 4, 1776', 'in the 19th century, when it became a symbol of the abolition of slavery', 'none of the above' ], 2],
+
+    [ 'In the Roy Rogers-Dale Evans Museum, you will find Roy and Dales stuffed horses. Roy\’s horse was named Trigger, which was Dales horse?', [ 'Buttermilk', 'Daisy', 'Scout', 'Tulip' ], 0 ],
+
+    [ 'The Daniel Boon museum at the home where he died can best be described how?', [ 'a log cabin in Kentucky', 'a two-story clapboard house in Tennessee', 'a four-story Georgian-style home in Missouri', 'a three story brick house in Arkansas' ], 2 ],
+
+    [ 'Which of the following items was owned by the fewest U.S. homes in 1990?', [ 'home computer', 'compact disk player', 'cordless phone', 'dishwasher' ], 1 ],
+
+    [ 'Who holds the record for the most victories in a row on the professional golf tour?', [ 'Jack Nicklaus', 'Arnold Palmer', 'Byron Nelson', 'Ben Hogan' ], 2 ],
+
+    [ 'Who is third behind Hank Aaron and Babe Ruth in major league career home runs?', [ 'Reggie Jackson', 'Harmon Killebrew', 'Willie Mays', 'Frank Robinson' ], 2 ],
+
+    [ 'In 1990, in what percentage of U.S. married couples did the wife earn more money than the husband?', [ '8', '18', '38', '58' ], 1 ],
+
+    [ 'During the 1980s for six consecutive years what breed of dog was the most popular in the U.S.?', [ 'cocker spaniel', 'German shepherd', 'Labrador retriever', 'poodle' ], 0 ],
+
+    [ 'In 1985, five percent of U.S. households had telephone answering machines. By 1990 what percentage of homes had answering machines?', [ '10 percent', '15 percent', '31 percent', '51 percent' ], 2 ],
+
+    [ 'The first black American pictured on a U.S. postage stamp was who?', [ 'Frederick Douglass', 'Booker T. Washington', 'Louis Armstrong', 'Joe Louis' ], 3 ],
+
+    ['What did the “D” in “D-Day” stand for?', [ 'doom', 'day', 'Dwight (Eisenhower)', 'Dunkirk' ], 1 ],
+
+    [ '20. The Brownie Box Camera introduced by Eastman Kodak in 1900 had a retail price of what?', [ '$1', '$5', '$10', '$20' ], 0 ],
+    
+    [ 'Which of these characters turned 40 years old in 1990?', [ 'Charlie Brown', 'Bugs Bunny', 'Mickey Mouse', 'Fred Flintstone' ], 0 ],
+
+    [ 'The Philadelphia mint started putting a “P” mint mark on quarters when?', [ '1960', '1980', '1970', 'never' ], 1 ],
+
+    ['Before becoming George Bush’s Secretary of Defense, what was Dick Cheney’s position?', [ 'congressman from Wyoming', 'Army general', 'governor of New Hampshire', 'secretary of defense under Ronald Reagan' ], 0 ],
+
+    [ 'When Mt. St. Helens erupted on May 18, 1980, how many people were killed?', [ '1', '57', '123', '571' ], 1 ],
+    
+    ['In J. Edgar Hoover, what did the J stand for?', [ 'James', 'John', 'Joseph', 'Jack' ], 1 ],
+
+    [ 'Florence Nightingale became known as “the Lady With the Lamp” during which war?', [ 'American Civil War' , 'Crimean War', 'World War I', 'Korean War' ], 1 ],
+    
+    [ 'What year was it that the Census Bureau first reported that a majority of new mothers were remaining in the new job market?', [ '1968', '1978', '1988', '2003' ], 2 ]
 ];
 
 function saveToLocal(key, value ) {
